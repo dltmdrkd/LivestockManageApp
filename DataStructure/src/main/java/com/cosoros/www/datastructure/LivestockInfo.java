6abdbc92f10eb11dbf15f90de7041d346276bcc8
cerik <dltmdrkd0704@gmail.com>
2018-03-15 18:13:43 +0900

    (+) Add packet analyzer

--- DataStructure/src/main/java/com/cosoros/www/datastructure/LivestockInfo.java
+++ DataStructure/src/main/java/com/cosoros/www/datastructure/LivestockInfo.java
@@ -1,8 +1,15 @@
 package com.cosoros.www.datastructure;
 
+import java.util.Date;
+
 /**
  * Created by dltmd on 3/15/2018.
  */
 
 public class LivestockInfo {
+    double _latitude;
+    double _longtitude;
+    double _altitude;
+    int    _satelliteCount;
+    Date   _timestamp;
 }
---
+++ DataStructure/src/main/java/com/cosoros/www/datastructure/PacketAnalyzer.java
@@ -0,0 +1,9 @@
+package com.cosoros.www.datastructure;
+
+/**
+ * Created by dltmd on 3/15/2018.
+ */
+
+public class PacketAnalyzer {
+
+}
