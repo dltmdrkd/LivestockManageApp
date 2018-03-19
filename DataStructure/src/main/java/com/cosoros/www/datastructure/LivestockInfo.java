ab0325d3e3025ccaef4bd2b0bc94a10a8351b30c
cerik <dltmdrkd0704@gmail.com>
2018-03-19 15:25:31 +0900

    (*) LivestockInfo modified

--- DataStructure/src/main/java/com/cosoros/www/datastructure/LivestockInfo.java
+++ DataStructure/src/main/java/com/cosoros/www/datastructure/LivestockInfo.java
@@ -7,9 +7,63 @@ import java.util.Date;
  */
 
 public class LivestockInfo {
-    double _latitude;
-    double _longtitude;
-    double _altitude;
-    int    _satelliteCount;
-    Date   _timestamp;
+    public LivestockInfo() {
+        _valid = false;
+    }
+
+    public LivestockInfo(LivestockInfo src) {
+        _valid = src.isValid();
+        _latitude = src.latitude();
+        _longtitude = src.longtitude();
+        _altitude = src.altitude();
+        _satelliteCount = src.satelliteCount();
+        _timestamp = src.timestamp();
+        _voltage = src.voltage();
+    }
+
+    private boolean _valid;
+    private double  _latitude;
+    private double  _longtitude;
+    private double  _altitude;
+    private int     _satelliteCount;
+    private Date    _timestamp;
+    private float   _voltage;
+
+    public boolean isValid() {
+        return _valid;
+    }
+
+    public double latitude() {
+        return _latitude;
+    }
+
+    public double longtitude() {
+        return _longtitude;
+    }
+
+    public double altitude() {
+        return _altitude;
+    }
+
+    public int satelliteCount() {
+        return _satelliteCount;
+    }
+
+    public Date timestamp() {
+        return _timestamp;
+    }
+
+    public float voltage() {
+        return _voltage;
+    }
+
+    public void setValues(double latitude, double longtitude, double altitude, int satelliteCount, Date timestamp, float voltage) {
+        _latitude = latitude;
+        _longtitude = longtitude;
+        _altitude = altitude;
+        _satelliteCount = satelliteCount;
+        _timestamp = timestamp;
+        _voltage = voltage;
+        _valid = true;
+    }
 }
