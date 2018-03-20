3d5dd93acbfbb4645ee34d8733c561f28fb88ca1
Jan <junhee.mongol@gmail.com>
2018-03-20 15:02:12 +0900

    (#1) Database 관리 테이블 생성

---
+++ Database/src/main/java/com/cosoros/www/database/DBTable.java
@@ -0,0 +1,160 @@
+package com.cosoros.www.database;
+
+/**
+ * Created by Jan on 2018-03-19.
+ *
+ * ls : Livestock
+ * ht : Health
+ * ds : Disease
+ * vc : Vaccine
+ *
+ */
+
+public class DBTable {
+    LwdHistory _lwdHistory = new LwdHistory();
+    LwdLsMatch _lwdLsMatch = new LwdLsMatch();
+    LwdList _lwdList = new LwdList();
+    LwdNotSent _lwdNotSent = new LwdNotSent();
+
+    LsInfo _lsInfo = new LsInfo();
+    LsVaccination _lsVaccination = new LsVaccination();
+    LsFamily _lsFamily = new LsFamily();
+
+    CodeLsType _codeLsType = new CodeLsType();
+    CodeLsKind _codeLsKind = new CodeLsKind();
+    CodeVaccine _codeVaccine = new CodeVaccine();
+}
+
+class LwdHistory {
+    String _table_name = "lwd_history";
+    String _lwd_id = "lwd_id";
+    String _ls_id = "ls_id";
+    String _data_origin = "data_origin";
+    String _data_latitude = "data_latitude";
+    String _data_longitude = "data_longitude";
+    String _data_altitude = "data_altitude";
+    String _data_satellite_cnt = "data_satellite_cnt";
+    String _data_time = "data_time";
+    String _data_battery = "data_battery";
+    String _primary_key = "PRIMARY KEY (ls_id, data_time)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _lwd_id + " TEXT NOT NULL, " + _ls_id + " TEXT, " + _data_origin + " TEXT, " +
+                    _data_latitude + " REAL, " + _data_longitude + " REAL, " + _data_altitude + " REAL, " +
+                    _data_satellite_cnt + " INTEGER, " + _data_time + " TEXT, " + _data_battery + " REAL, " +
+                    _primary_key + ");";
+}
+
+class LwdLsMatch {
+    String _table_name = "lwd_ls_match";
+    String _lwd_id = "lwd_id";
+    String _ls_id = "ls_id";
+    String _lwd_time = "lwd_time";
+    String _primary_key = "PRIMARY KEY (lwd_id, ls_id)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _lwd_id + " TEXT, " + _ls_id + " TEXT, " + _lwd_time + " TEXT, " +
+                    _primary_key + ");";
+}
+class LwdList {
+    String _table_name = "lwd_list";
+    String _lwd_id = "lwd_id";
+    String _lwd_name = "lwd_name";
+    String _primary_key = "PRIMARY KEY (lwd_id, lwd_name)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _lwd_id + " TEXT, " + _lwd_name + " TEXT, " +
+                    _primary_key + ");";
+}
+
+class LwdNotSent {
+    String _table_name = "lwd_list";
+    String _lwd_id = "lwd_id";
+    String _lwd_time = "lwd_time";
+    String _primary_key = "PRIMARY KEY (lwd_id, lwd_time)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _lwd_id + " TEXT, " + _lwd_time + " TEXT, " +
+                    _primary_key + ");";
+}
+
+class LsInfo {
+    String _table_name = "ls_info";
+    String _ls_id = "ls_id";
+    String _ls_type = "ls_type";
+    String _ls_kind = "ls_kind";
+    String _ls_birth = "ls_birth";
+    String _ls_sex = "ls_sex";
+    String _ls_name = "ls_name";
+    String _ls_weight = "ls_weight";
+    String _primary_key = "PRIMARY KEY (ls_id)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _ls_id + " TEXT , " + _ls_type + " INTEGER, " + _ls_kind + " INTEGER, " +
+                    _ls_birth + " TEXT, " + _ls_sex + " INTEGER, " + _ls_name + " TEXT, " + _ls_weight + " REAL, " +
+                    _primary_key + ");";
+}
+
+class LsVaccination {
+    String _table_name = "ls_vaccination";
+    String _ls_id = "ls_id";
+    String _ls_code = "ls_code";
+    String _ls_date = "ls_date";
+    String _primary_key = "PRIMARY KEY (lwd_id, lwd_time, ls_date)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _ls_id + " TEXT, " + _ls_code + " INTEGER, " + _ls_date + " TEXT, " +
+                    _primary_key + ");";
+}
+
+class LsFamily {
+    String _table_name = "ls_family";
+    String _ls_id = "ls_id";
+    String _ls_father = "ls_father";
+    String _ls_mother = "ls_mother";
+    String _primary_key = "PRIMARY KEY (ls_id, ls_father, ls_mother)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _ls_id + " TEXT, " + _ls_father + " TEXT, " + _ls_mother + " TEXT, " +
+                    _primary_key + ");";
+}
+
+class CodeLsType {
+    String _table_name = "code_ls_type";
+    String _ls_type_code = "ls_type_code";
+    String _ls_type_name = "ls_type_name";
+//    String _primary_key = "PRIMARY KEY (lwd_id, lwd_time)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _ls_type_code + " TEXT, " + _ls_type_name + " TEXT);";
+}
+
+class CodeLsKind {
+    String _table_name = "code_ls_kind";
+    String _ls_kind_code = "ls_kind_code";
+    String _ls_kind_name = "ls_kind_name";
+//    String _primary_key = "PRIMARY KEY (lwd_id, lwd_time)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _ls_kind_code + " TEXT, " + _ls_kind_name + " TEXT);";
+}
+
+class CodeVaccine {
+    String _table_name = "code_vaccine";
+    String _vc_code = "vc_code";
+    String _vc_name = "vc_name";
+//    String _primary_key = "PRIMARY KEY (lwd_id, lwd_time)";
+
+    String _create_table =
+            "CREATE TABLE IF NOT EXISTS " + _table_name + "(" +
+                    _vc_code + " TEXT, " + _vc_name + " TEXT);";
+}
--- Database/src/main/java/com/cosoros/www/database/Database.java
+++ Database/src/main/java/com/cosoros/www/database/Database.java
@@ -1,8 +1,129 @@
 package com.cosoros.www.database;
 
-/**
- * Created by dltmd on 3/15/2018.
- */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-public class Database {
+import java.util.Date;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final int _DATABASE_VERSION = 2;
+    private static final String _DB_NAME = "nomad_lwd.db";
+    private static final DBTable _table = new DBTable();
+
+    public Database(Context context) {
+        super(context, _DB_NAME, null, _DATABASE_VERSION);
+        Log.d("DATABASE", "DB CONSTRUCT START");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("DATABASE", "DB CREATE START");
+        db.execSQL(_table._lwdHistory._create_table);
+        db.execSQL(_table._lwdLsMatch._create_table);
+        db.execSQL(_table._lwdList._create_table);
+        db.execSQL(_table._lwdNotSent._create_table);
+
+        db.execSQL(_table._lsInfo._create_table);
+        db.execSQL(_table._lsVaccination._create_table);
+        db.execSQL(_table._lsFamily._create_table);
+
+        db.execSQL(_table._codeLsType._create_table);
+        db.execSQL(_table._codeLsKind._create_table);
+        db.execSQL(_table._codeVaccine._create_table);
+
+        Log.d("DATABASE", "DB CREATE DONE");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d("DATABASE", "DB UPDATE START");
+
+        db.execSQL("DROP TABLE IF EXISTS " + _table._lwdHistory._table_name);
+        db.execSQL("DROP TABLE IF EXISTS " + _table._lwdLsMatch._table_name);
+        db.execSQL("DROP TABLE IF EXISTS " + _table._lwdList._create_table);
+        db.execSQL("DROP TABLE IF EXISTS " + _table._lwdNotSent._create_table);
+
+        db.execSQL("DROP TABLE IF EXISTS " + _table._lsInfo._create_table);
+        db.execSQL("DROP TABLE IF EXISTS " + _table._lsVaccination._create_table);
+        db.execSQL("DROP TABLE IF EXISTS " + _table._lsFamily._create_table);
+
+        db.execSQL("DROP TABLE IF EXISTS " + _table._codeLsType._create_table);
+        db.execSQL("DROP TABLE IF EXISTS " + _table._codeLsKind._create_table);
+        db.execSQL("DROP TABLE IF EXISTS " + _table._codeVaccine._create_table);
+        onCreate(db);
+
+        Log.d("DATABASE", "DB UPDATE DONE");
+    }
+
+    public void add() {
+        Log.d("DATABASE", "DB INSERT START");
+
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        Date t = new Date();
+//        String sql = "INSERT INTO lwd_history (lwd_id, ls_id, data_time) VALUES ('101F', 'A0001M', '" + t.getTime() + "');";
+//
+//        db.execSQL(sql);
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        Date t = new Date();
+        values.put(_table._lwdHistory._lwd_id, "102F");
+        values.put(_table._lwdHistory._ls_id, "A0001M");
+        values.put(_table._lwdHistory._data_time, t.getTime());
+
+        db.insert(_table._lwdHistory._table_name, null, values);
+        Log.d("DATABASE", "DB INSERT DONE");
+    }
+
+    public void read() {
+        Log.d("DATABASE", "DB READ START");
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String sql2 = "SELECT lwd_id, ls_id, data_time FROM lwd_history ORDER BY data_time;";
+        Cursor cursor = db.rawQuery(sql2, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+
+                Log.d("DATABASE", "lwd_id : " + cursor.getString(0));
+                Log.d("DATABASE", "ls_id : " + cursor.getString(1));
+                Log.d("DATABASE", "data_time : " + cursor.getString(2));
+
+            } while (cursor.moveToNext());
+        }
+        Log.d("DATABASE", "DB READ DONE");
+    }
+
+//    public List<Drink> getAll() {
+//
+//        DBTable table = new DBTable();
+//        String name = table._lwdHistory._name;
+//
+//
+//        List<Drink> drinkList = new ArrayList<Drink>();
+//
+//        String SELECT_ALL = "SELECT * FROM " + TABLE_DRINK;
+//
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        Cursor cursor = db.rawQuery(SELECT_ALL, null);
+//
+//        if (cursor.moveToFirst()) {
+//            do {
+//                Drink drink = new Drink();
+//                drink.setId(Integer.parseInt(cursor.getString(0)));
+//                drink.setName(cursor.getString(1));
+//                drinkList.add(drink);
+//            } while (cursor.moveToNext());
+//        }
+//
+//        return drinkList;
+//    }
+
 }
